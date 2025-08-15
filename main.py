7616f1e7e0e205c13561973ad20425c4227e709f
Pedram Babakhani <pedrambbk@email.com>
2025-08-15 16:59:45 +0200

    Initial Cloud Run deployment

--- main.py
+++ main.py
@@ -468,6 +468,9 @@ def gaiax_descriptor():
     }
     return descriptor
 
+
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", "8000")), reload=True)
